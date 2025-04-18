c984b643aeaf4edb148c186280b01f27fd3d034c
ghosstyx <timurbakhromov@gmail.com>
2025-04-18 16:48:46 +0500

    Kanban Board update

--- help_desk/admin.py
+++ help_desk/admin.py
@@ -6,9 +6,7 @@ from .models import Project, Task
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
     list_display = ('title', 'owner')
-    raw_id_fields = ('owner',)
 
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
     list_display = ('title', 'assignee')
-    raw_id_fields = ('assignee',)
