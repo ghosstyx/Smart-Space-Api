f3390377ea0882381395fea27f7cc1a02e87bdab
ghosstyx <timurbakhromov@gmail.com>
2025-04-18 15:55:18 +0500

    Kanban Board update

--- config/settings/base.py
+++ config/settings/base.py
@@ -1,7 +1,9 @@
 import os
 from datetime import timedelta
 from pathlib import Path
-from decouple import config
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -12,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'jndnjwokdwmdc12445hjbdcm^$#Q'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 
 # Application definition
